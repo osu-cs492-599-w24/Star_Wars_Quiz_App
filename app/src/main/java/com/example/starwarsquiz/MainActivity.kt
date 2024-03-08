ab91e9cbc4e4501d47a65258335ef573e3a68404
ncknelson <nelsonn3@oregonstate.edu>
2024-03-07 23:37:12 -0800

    Improved API service class

--- app/src/main/java/com/example/starwarsquiz/MainActivity.kt
+++ app/src/main/java/com/example/starwarsquiz/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import com.example.starwarsquiz.data.SWAPICharacterResults
 import com.example.starwarsquiz.data.SWAPIService
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,19 +19,21 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         val mainTV: TextView = findViewById(R.id.tv_main_activity)
-
         mainTV.text = "Star Wars App"
         doCharacterSearch(1, 10)
     }
 
     private fun doCharacterSearch(page: Int, limit: Int) {
-        swapiService.loadCharacters(page, limit).enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+        swapiService.loadCharacters(page, limit).enqueue(object : Callback<SWAPICharacterResults> {
+            override fun onResponse(call: Call<SWAPICharacterResults>, response: Response<SWAPICharacterResults>) {
                 Log.d("Main Activity", "Status code: ${response.code()}")
                 Log.d("Main Activity", "Body: ${response.body()}")
+                if (response.isSuccessful) {
+                    Log.d("Main Activity", "Name: ${response.body()?.results?.get(0)!!.name}")
+                }
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<SWAPICharacterResults>, t: Throwable) {
                 Log.d("Main Activity", "Error making call: ${t.message}")
             }
 
--- app/src/main/java/com/example/starwarsquiz/data/SWAPIService.kt
+++ app/src/main/java/com/example/starwarsquiz/data/SWAPIService.kt
@@ -2,23 +2,23 @@ package com.example.starwarsquiz.data
 
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 interface SWAPIService {
     @GET ("people")
     fun loadCharacters(
         @Query("page") page: Int = 1,
         @Query("limit") limit: Int = 10
-    ): Call<String>
+    ): Call<SWAPICharacterResults>
 
     companion object {
         private const val BASE_URL = "https://www.swapi.tech/api/"
         fun create(): SWAPIService {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(MoshiConverterFactory.create())
                 .build()
                 .create(SWAPIService::class.java)
         }
