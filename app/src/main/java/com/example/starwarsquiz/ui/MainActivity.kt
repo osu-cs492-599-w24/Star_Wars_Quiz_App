5d34b31d3afd82bae7df708bf6f91aafb75a7e05
ncknelson <nelsonn3@oregonstate.edu>
2024-03-08 11:56:38 -0800

    Started Character ViewModel

--- app/src/main/java/com/example/starwarsquiz/ui/MainActivity.kt
+++ app/src/main/java/com/example/starwarsquiz/ui/MainActivity.kt
@@ -1,14 +1,10 @@
-package com.example.starwarsquiz
+package com.example.starwarsquiz.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.starwarsquiz.data.SWAPICharacterResults
+import com.example.starwarsquiz.R
 import com.example.starwarsquiz.data.SWAPIService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private val swapiService = SWAPIService.create()
