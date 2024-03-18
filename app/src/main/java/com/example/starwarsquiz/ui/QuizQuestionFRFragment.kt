ee649609d681b1691a3c49f315289bd25f4bafcc
Jonah Cadiz <cadizjo@oregonstate.edu>
2024-03-18 13:29:33 -0700

    created quiz question FR type fragment

---
+++ app/src/main/java/com/example/starwarsquiz/ui/QuizQuestionFRFragment.kt
@@ -0,0 +1,7 @@
+package com.example.starwarsquiz.ui
+
+import androidx.fragment.app.Fragment
+import com.example.starwarsquiz.R
+
+class QuizQuestionFRFragment : Fragment(R.layout.fragment_quiz_question_fr) {
+}
