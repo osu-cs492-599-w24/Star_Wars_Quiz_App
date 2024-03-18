bd498d355251500406dd9487a0615bd60934dc77
Jonah Cadiz <cadizjo@oregonstate.edu>
2024-03-18 13:00:36 -0700

    created quiz question MC type fragment

---
+++ app/src/main/java/com/example/starwarsquiz/ui/QuizQuestionMCFragment.kt
@@ -0,0 +1,8 @@
+package com.example.starwarsquiz.ui
+
+import androidx.fragment.app.Fragment
+import com.example.starwarsquiz.R
+
+class QuizQuestionMCFragment : Fragment(R.layout.fragment_quiz_question_mc){
+
+}
