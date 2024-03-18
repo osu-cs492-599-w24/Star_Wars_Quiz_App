4198ec5ceee85616dc6fb128e03a4e8fa9ed2eef
Jonah Cadiz <cadizjo@oregonstate.edu>
2024-03-18 14:43:12 -0700

    created quiz results fragment

---
+++ app/src/main/java/com/example/starwarsquiz/ui/QuizResultsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.starwarsquiz.ui
+
+import androidx.fragment.app.Fragment
+import com.example.starwarsquiz.R
+
+class QuizResultsFragment : Fragment(R.layout.fragment_quiz_results) {
+}
