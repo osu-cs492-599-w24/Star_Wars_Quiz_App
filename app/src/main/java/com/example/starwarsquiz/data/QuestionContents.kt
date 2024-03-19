9e47aa082f974fc67783185b023ed51133d6eee8
Robert Vincent <vincerob@oregonstate.edu>
2024-03-18 17:56:20 -0700

    Serialize QuestionContents.kt

--- app/src/main/java/com/example/starwarsquiz/data/QuestionContents.kt
+++ app/src/main/java/com/example/starwarsquiz/data/QuestionContents.kt
@@ -1,8 +1,10 @@
 package com.example.starwarsquiz.data
 
+import java.io.Serializable
+
 data class QuestionContents(
     val quizNumber: Int,
     val question: String,
     val correctAnswer: String,
     val answerChoices: List<String>?
-)
+) : Serializable
