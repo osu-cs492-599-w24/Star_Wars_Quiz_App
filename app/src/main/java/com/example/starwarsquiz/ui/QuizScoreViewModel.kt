a368e1f3bd95bda4d799e31c867ac0ffb8dadb8e
Jonah Cadiz <cadizjo@oregonstate.edu>
2024-03-18 18:06:52 -0700

    created quiz score repository and view model

---
+++ app/src/main/java/com/example/starwarsquiz/ui/QuizScoreViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.starwarsquiz.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.starwarsquiz.data.AppDatabase
+import com.example.starwarsquiz.data.QuizScoreEntity
+import com.example.starwarsquiz.data.QuizScoreRepository
+import kotlinx.coroutines.launch
+
+class QuizScoreViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = QuizScoreRepository(
+        AppDatabase.getInstance(application).quizScoreDao()
+    )
+
+    val highestScore = repository.getHighestScore().asLiveData()
+
+    fun addQuizScore(quizScore: QuizScoreEntity) {
+        viewModelScope.launch {
+            repository.insertQuizScore(quizScore)
+        }
+    }
+
+    fun updateQuizScore(runId: Int, newScore: Int) {
+        viewModelScope.launch {
+            repository.updateQuizScore(runId, newScore)
+        }
+    }
+}
