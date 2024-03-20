bf18135e0742620cf4fce9b02b0f1ab530b3b0f9
ncknelson <nelsonn3@oregonstate.edu>
2024-03-19 19:24:00 -0700

    Can Visit and Return to Landing Page from Score History

--- app/src/main/java/com/example/starwarsquiz/ui/ScoreHistoryFragment.kt
+++ app/src/main/java/com/example/starwarsquiz/ui/ScoreHistoryFragment.kt
@@ -2,8 +2,10 @@ package com.example.starwarsquiz.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.starwarsquiz.R
@@ -12,13 +14,22 @@ class ScoreHistoryFragment : Fragment(R.layout.fragment_score_history) {
     private val quizScoreViewModel: QuizScoreViewModel by viewModels()
 
     private lateinit var scoresRV: RecyclerView
+    private lateinit var homeButton: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        homeButton = view.findViewById(R.id.button_home)
+
         scoresRV = view.findViewById(R.id.rv_scores)
         scoresRV.layoutManager = LinearLayoutManager(requireContext())
         scoresRV.setHasFixedSize(true)
+
+        // Home button goes to landing page
+        homeButton.setOnClickListener {
+            val action = ScoreHistoryFragmentDirections.navigateToLandingPage()
+            findNavController().navigate(action)
+        }
     }
 
 }
