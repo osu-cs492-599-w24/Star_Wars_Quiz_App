2988c71d49e9bc9cf53a3849f37113c9d7bae8b5
Username-Jay-is-not-available <greenjaa@oregonstate.edu>
2024-03-20 22:45:41 -0700

    Modified QuizQuestionMCFragment to have questions

--- app/src/main/java/com/example/starwarsquiz/ui/QuizQuestionMCFragment.kt
+++ app/src/main/java/com/example/starwarsquiz/ui/QuizQuestionMCFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -14,12 +15,17 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.starwarsquiz.R
 import com.example.starwarsquiz.data.QuestionContents
+import com.example.starwarsquiz.data.SWAPICharacter
+import com.example.starwarsquiz.data.SWAPIPlanet
 
 class QuizQuestionMCFragment : Fragment(R.layout.fragment_quiz_question_mc){
     private val args: QuizQuestionMCFragmentArgs by navArgs()
 
     // declare necessary view models here
     private val quizScoreViewModel: QuizScoreViewModel by viewModels()
+
+    private val planetsViewModel: SWAPIPlanetViewModel by viewModels()
+    private val resultViewModel: SWAPICharacterViewModel by viewModels()
 
     // stores list of possible answers to this question
     private val adapter = MCAnswerListAdapter()
@@ -33,6 +39,12 @@ class QuizQuestionMCFragment : Fragment(R.layout.fragment_quiz_question_mc){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        planetsViewModel.loadSWAPIPlanets(1, 40)
+        var planet = listOf<SWAPIPlanet>()
+
+        resultViewModel.loadSWAPICharacters(1, 80)
+        var people = listOf<SWAPICharacter>()
 
         questionNumTV = view.findViewById(R.id.tv_quiz_question_num)
         currentScoreTV = view.findViewById(R.id.tv_quiz_current_score)
@@ -91,6 +103,25 @@ class QuizQuestionMCFragment : Fragment(R.layout.fragment_quiz_question_mc){
 
         // Next button goes to next question
         nextButton.setOnClickListener {
+
+            planetsViewModel.planetList.observe(viewLifecycleOwner) { planetList->
+                if(planetList != null){
+                    planet = planetList
+                }
+                else{
+                    Log.d("LandingPageFragment", "planetList is empty")
+                }
+            }
+
+            resultViewModel.characterResults.observe(viewLifecycleOwner) { characterResults->
+                if(characterResults != null){
+                    people = characterResults
+                }
+                else{
+                    Log.d("LandingPageFragment", "characterResults is empty")
+                }
+            }
+
             val nextScore = if (true) { // TODO: Replace with actual answer comparison
                 // If answer is correct, increment score
                 args.questionContents.currentScore + 1
@@ -99,16 +130,166 @@ class QuizQuestionMCFragment : Fragment(R.layout.fragment_quiz_question_mc){
                 args.questionContents.currentScore
             }
 
-            val newArgs = QuestionContents(
-                args.questionContents.quizNumber + 1,
-                nextScore,
-                "REPLACE ME WITH AN ACTUAL QUESTION",
-                "ANSWER 1",
-                listOf("ANSWER 1", "ANSWER 2", "ANSWER 3", "ANSWER 4")
-            )
 
-            val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
-            findNavController().navigate(action)
+
+            //58-1 or -2 bc of people/17?
+
+            val nextQ : Int = args.questionContents.quizNumber + 1
+
+            val newArgs : QuestionContents
+
+            when (nextQ) {
+                1 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "What planet is Darth Maul from?",
+                        planet[36-1].name,
+                        listOf(planet[1-1].name, planet[36-1].name, planet[14-1].name, planet[8-1].name)
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                2 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "Who was Starkiller's master?",
+                        people[4-1].name,
+                        listOf(people[4-1].name, people[11-1].name, people[10-1].name, people[4-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                3 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "Who was the jedi that discovered Ahsoka?",
+                        people[58-1].name,
+                        listOf(people[11-1].name, people[53-1].name, people[10-1].name, people[58-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                4 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "Whose DNA was used to create the clone troopers?",
+                        people[69-1].name,
+                        listOf(people[69-1].name, people[72-1].name, people[22-1].name, people[67-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                5 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "Who was the only unaltered clone?",
+                        people[22-1].name,
+                        listOf(people[69-1].name, people[72-1].name, people[22-1].name, people[67-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                6 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "Who was Qui Gon Jinn's master?",
+                        people[67-1].name,
+                        listOf(people[1-1].name, people[20-1].name, people[67-1].name, people[10-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                7 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "What planet was Palpatine from?",
+                        planet[8-1].name,
+                        listOf(planet[1-1].name, planet[9-1].name, planet[7-1].name, planet[8-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                8 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "What planet was Starkiller from?",
+                        planet[14-1].name,
+                        listOf(planet[9-1].name, planet[14-1].name, planet[10-1].name, planet[5-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                9 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "Who inquired about the droid attack on the wookies?",
+                        people[52-1].name,
+                        listOf(people[52-1].name, people[57-1].name, people[58-1].name, people[51-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+                10 -> {
+
+                    newArgs = QuestionContents(
+                        args.questionContents.quizNumber,
+                        nextScore,
+                        "Hello there",
+                        people[10-1].name,
+                        listOf(people[79-1].name, people[11-1].name, people[1-1].name, people[10-1].name)
+
+                    )
+
+                    val action = QuizQuestionMCFragmentDirections.navigateToQuizQuestionFr(newArgs)
+                    findNavController().navigate(action)
+
+                }
+            }
+
         }
 
 
